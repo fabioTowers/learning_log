c6319c16b030baebbec96c1eb75db1b5283e9bf3
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-12 15:20:07 -0300

    Adicionando o modelo Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,3 +21,25 @@ class Topic(models.Model):
     def __str__(self):
         """Devolve uma representação em string do modelo."""
         return self.text
+
+# Mais um modelo de dado, mais uma tabela no banco
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto."""
+    # Possui uma chave-estrangeira, referenciando o conteúdo dessa tabela com Topic
+    topic = models.ForeignKey(Topic)
+    # Atributo do tipo TextField, que não precisa de limite de tamanho
+    text = models.TextField
+    # Atributo que adiciona etiqueta de tempo ao registro
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    #Meta informação para o django: para se referir a mais de uma entrada o django usara entries
+    # e não entrys (que seria o default)
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    # Quais informações devem ser mostradas quando entradas individuais forem referenciadas
+    # A entrada pode ser longa, então definimos que exiba apenas os 50 primeiros caracteres da
+    # string e com reticiências para deixar clara que foi cortado um trecho
+    def __str__(self):
+       """Devolve uma representação em string do modelo."""
+       return self.text[:50] + "..."
