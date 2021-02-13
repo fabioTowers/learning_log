cdbbc1998cf4fc08c276498e23c1e25fc8deb8b6
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-13 15:09:50 -0300

    Criando a view da página inicial

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Em urls.py definimos a view index, que está definida abaixo
+# o request será passado a essa função, que por sua vez chama a 
+# função render() que recebe o request e uma página que deve ser retornada
+def index(request):
+    """A página inicial de Learning Log"""
+    return render(request, 'learning_logs/index.html')
