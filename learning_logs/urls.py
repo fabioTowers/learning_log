3759a8bbb691facdb967e015ffed1380a664a522
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-16 15:01:40 -0300

    Adicionado o mapeamente de URL para a página específica a um tópico

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,4 +15,13 @@ urlpatterns = [
 
     # Mostra todos os assuntos
     url(r'^topics/$', views.topics, name='topics'),
+
+    # Página de detalhes para um único assunto
+    #O primeiro parâmetro é um regex:
+    #r interprete a string como pura
+    #/(?P<topic_id>\d+)/ entre as barras teremos um inteiro e armazena o valor em topic_id
+    #\d+ indica qualquer quantidade de dígitos que apareça entre as barras
+    #Quando um endereço de requisição corresponde so padrão, será chamada a função de view topic()
+    #com o argumento topic_id
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
