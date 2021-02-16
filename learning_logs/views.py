c9a629e92e9de44269a9f6cc339506de49e2c105
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-15 21:15:36 -0300

    Definindo a view de assuntos

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# Importando o modelo de dados necessário
+from .models import Topic
+
 # Create your views here.
 #Em urls.py definimos a view index, que está definida abaixo
 # o request será passado a essa função, que por sua vez chama a 
@@ -7,3 +10,19 @@ from django.shortcuts import render
 def index(request):
     """A página inicial de Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    # Consulta ao banco de dados, solicitando os objetos Topic
+    #ordenados pelo atributo 'date_added', o queryset de 
+    #resposta é armazenado em topics
+    topics = Topic.objects.order_by('date_added')
+
+    # Abaixo temos o contexto: as chaves são os nomes que 
+    #serão usados no template para acessar, e os valores são 
+    #os dados que vamos enviar 
+    context = {'topics': topics}
+
+    # Passamos ao método render() o request, o caminho do 
+    #template, e os dados a serem exibidos (context)
+    return render(request, 'learning_logs/topics.html', context)
