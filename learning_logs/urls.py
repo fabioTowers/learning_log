67875182602e9f847dec32fa8743061b76a9617f
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-25 13:25:48 -0300

    Adicionado o padrão de URL para requisições de inserção de nova entrada

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -28,4 +28,11 @@ urlpatterns = [
     # Padrão de URL para página de novos tópicos (assuntos)
     #a solicitação de view será enviada para a função new_topic
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # Definição da URL da página para adicionar uma nova entrada em um assunto
+    #note que essa página é 'filha' de uma página de assuntos
+    #novamente adicionamos um valor numérico na variável topic_id
+    #quando uma URL com esse padrão for requisitada, Django enviará a requisição
+    #e o ID do assunto para a função de view new_entry()
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry), name='new_entry'),
 ]
