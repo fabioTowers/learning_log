a9fbf50648502810d2abc79f1c5af7a801bb9b60
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-02-25 13:18:56 -0300

    Definido o ModelForm para inserção de nova entrada

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,8 +1,9 @@
 # Importando o módulo forms
 from django import forms
 
-# importando o modelo Topic que definimos
-from .models import Topic
+# importando o modelo Topic que definimos e o modelo Entry
+from .models import Topic, Entry
+
 
 # Definimos a classe TopicForms quer herda de forms.modelForm
 class TopicForm(forms.ModelForm):
@@ -15,3 +16,18 @@ class TopicForm(forms.ModelForm):
         fields = ['text']
         # Especifica ao django para não gerar um rótulo para o campo
         labels = {'text': ''}
+
+# Essa classe herda de ModelForm
+class EntryForm(forms.ModelForm):
+    # Classe Meta aninhada que lista o modelo no qual ela está baseada
+    #e o campo a ser incluído no formulário
+    class Meta:
+        model = EntryForm
+        fields = ['text']
+        # Rótulo vazio para text
+        labels = {'text': ''}
+        # Widget é um elemento de formulário HTML, com ele podemos 
+        #sobrescrever atributos default de um elemento.
+        # Nesse caso estamos definindo que text area terá 80 colunas 
+        #ao invés das 40 padrão.
+        widgets = {'text': forms.Textarea(attrs={'cols':80})}
