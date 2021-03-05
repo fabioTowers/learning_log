e0cb2738b2592c2cb010054478902573eb3b14a9
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-03-05 17:07:20 -0300

    Criada a função de view para atender as requisições de logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+# Para importa a função logout()
+from django.contrib.auth import logout
+
 # Create your views here.
+
+def logout_view(request):
+    """Faz logout do usuário."""
+    logout(request)
+    # Redireciona para a página inicial
+    return HttpResponseRedirect(reverse('learning_logs:index'))
