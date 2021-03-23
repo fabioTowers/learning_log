4b4d67be6bb74dab91795f02390d1a41638a4141
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-03-23 17:52:40 -0300

    Configurado para deploy no heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -138,3 +138,22 @@ BOOTSTRAP3 = {
     #oferecidos pelos templates do Bootstrap
     'include_jquery': True,
 }
+
+# Configurações para o Heroku
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # Honra o cabeçalho 'X-Forwarded-Proto' para request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Cabeçalhos para permitir todos os hosts
+    ALLOWED_HOSTS = [*]
+
+    # Configuração de recursos estáticos
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
