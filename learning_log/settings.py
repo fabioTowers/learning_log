0893f14ce5226cd4a5ab5a52819bafa0bd5cad62
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-03-23 20:27:33 -0300

    Correção em STATIC_ROOT

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -155,7 +155,7 @@ if os.getcwd() == '/app':
 
     # Configuração de recursos estáticos
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
