3dfc2b296e7b0ee364067140eddc3bf8c3dba97f
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-03-23 17:58:28 -0300

    Correção de erro de sintaxe em settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -149,7 +149,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Cabeçalhos para permitir todos os hosts
-    ALLOWED_HOSTS = [*]
+    ALLOWED_HOSTS = ['*']
 
     # Configuração de recursos estáticos
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
