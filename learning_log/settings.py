2abeed511006090dd19c7202508dbdb3064f0c13
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-03-23 21:04:52 -0300

    Correção de erro de identação

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -161,6 +161,6 @@ BOOTSTRAP3 = {
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )'''
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     
-    django_heroku.settings(locals())
+django_heroku.settings(locals())
